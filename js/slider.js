d1f570e026ed1987eccd67b174858097de556275
Max Vasilishin <maxv8199@gmail.com>
2020-01-31 11:39:21 +0200

    refactored slider

--- js/slider.js
+++ js/slider.js
@@ -1,49 +1,58 @@
 let carouselWrapper = document.querySelector("#carouselWrapper");
 let items = document.querySelectorAll(".item-inner");
+/////////////////////////////////////////////////////////////////
 let slidesToShow = Math.round(carouselWrapper.clientWidth / items[0].clientWidth);
 let itemWidth = (carouselWrapper.clientWidth / slidesToShow) * 100 / carouselWrapper.clientWidth;
 let currentItem = 0;
+////////////////////////////////////////////////////////////////
 let leftBtn = document.querySelector(".left");
 let rightBtn = document.querySelector(".right");
-
+let select = document.querySelector("select");
+///////////////////////////////////////////////////////////////func
+function move(movingIndex){
+    carouselWrapper.style.transform = `translateX(${movingIndex*itemWidth}%)`;
+}
 function moveLeft() {
     if(currentItem < 0 ){
         currentItem++;
-        carouselWrapper.style.transform = `translateX(${currentItem*itemWidth}%)`;
-        console.log("ml " + currentItem);
+        move(currentItem);
     }
 }
 function moveRight(){
     if(currentItem > slidesToShow - items.length) {
         currentItem--;
-        carouselWrapper.style.transform = `translateX(${currentItem * itemWidth}%)`;
-        console.log("mr " + currentItem);
+        move(currentItem);
     }
 }
 
-
-    $(select).change(function () {
+function scrollTo(){
     carouselWrapper.scrollIntoView({
         behavior:"smooth"
     });
-
     let selectedIndex = Number.parseInt(select.options[select.selectedIndex].value);
-        console.log("select " + selectedIndex);
-        if(selectedIndex >= items.length - slidesToShow || currentItem > selectedIndex ){
-            currentItem--;
-            carouselWrapper.style.transform = `translateX(${-(items.length - slidesToShow) * itemWidth}%)`;
-            console.log("val if2 " + currentItem);
+    if(selectedIndex === -currentItem){
+        return 0;
+    }
+    if(selectedIndex === 0){
+        currentItem = 0;
+        move(currentItem);
+    }
+    if(selectedIndex >= items.length - slidesToShow && selectedIndex + currentItem !== 2){
+        if(selectedIndex + currentItem !==1){
+            currentItem++;
         }
-        if(selectedIndex <= items.length -slidesToShow){
-            currentItem--;
-            carouselWrapper.style.transform = `translateX(${-selectedIndex * itemWidth}%)`;
-            console.log("val else " + currentItem);
-        }
-        if(currentItem < 0){
-            return;
-        }
-    });
+        move((items.length - slidesToShow)*-1);
+        currentItem-=(items.length - slidesToShow);
+    }
+    if(selectedIndex <= items.length - slidesToShow && selectedIndex!==0){
+        currentItem--;
+        move(-selectedIndex);
+    }
+}
 
-
-leftBtn.addEventListener("click",moveLeft);
-rightBtn.addEventListener("click",moveRight);
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM LOADED");
+    select.addEventListener("change",scrollTo);
+    leftBtn.addEventListener("click",moveLeft);
+    rightBtn.addEventListener("click",moveRight);
+});
